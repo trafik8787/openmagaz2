88a8708a301291dbe6ee6b1bac4bff32bbe8e867
Shtirlic <trafik8787@gmail.com>
2016-02-20 14:12:27 +0200

    create core canary

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://canary.webremote.net/admin/');
+define('HTTP_CATALOG', 'http://canary.webremote.net/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://canary.webremote.net/admin/');
+define('HTTPS_CATALOG', 'http://canary.webremote.net/');
+
+// DIR
+define('DIR_APPLICATION', '/home/canary/www/admin/');
+define('DIR_SYSTEM', '/home/canary/www/system/');
+define('DIR_LANGUAGE', '/home/canary/www/admin/language/');
+define('DIR_TEMPLATE', '/home/canary/www/admin/view/template/');
+define('DIR_CONFIG', '/home/canary/www/system/config/');
+define('DIR_IMAGE', '/home/canary/www/image/');
+define('DIR_CACHE', '/home/canary/www/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/canary/www/system/storage/download/');
+define('DIR_LOGS', '/home/canary/www/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/canary/www/system/storage/modification/');
+define('DIR_UPLOAD', '/home/canary/www/system/storage/upload/');
+define('DIR_CATALOG', '/home/canary/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'canary');
+define('DB_PASSWORD', 'r2rkPdWOGStuNO8');
+define('DB_DATABASE', 'canary');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://canary.webremote.net/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://canary.webremote.net/');
+
+// DIR
+define('DIR_APPLICATION', '/home/canary/www/catalog/');
+define('DIR_SYSTEM', '/home/canary/www/system/');
+define('DIR_LANGUAGE', '/home/canary/www/catalog/language/');
+define('DIR_TEMPLATE', '/home/canary/www/catalog/view/theme/');
+define('DIR_CONFIG', '/home/canary/www/system/config/');
+define('DIR_IMAGE', '/home/canary/www/image/');
+define('DIR_CACHE', '/home/canary/www/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/canary/www/system/storage/download/');
+define('DIR_LOGS', '/home/canary/www/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/canary/www/system/storage/modification/');
+define('DIR_UPLOAD', '/home/canary/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'canary');
+define('DB_PASSWORD', 'r2rkPdWOGStuNO8');
+define('DB_DATABASE', 'canary');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
