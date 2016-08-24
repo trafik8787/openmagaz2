a7f963618d46ec998e51cbfcacefd668d3538fc8
Shtirlic <trafik8787@gmail.com>
2016-08-24 14:25:56 +0300

    parse solitaire engagement rings

--- admin/controller/module/exel_parser.php
+++ admin/controller/module/exel_parser.php
@@ -61,25 +61,25 @@ class ControllerModuleExelParser extends Controller {
         );
 
         //ENGAGEMENT RINGS
-//        $this->category_arr = array(
-//            'Three-stone' => 63,
-//            'Vintage' => 66,
-//            'Bezel set' => 64,
-//            'Halo' => 65,
-//            'Bypass' => 62,
-//            'Pave' => 60,
-//            'Solitaire' => 59,
-//            'Modern'    => 89,
-//            'Channel'   => 61
-//        );
+        $this->category_arr = array(
+            'Three-stone' => 63,
+            'Vintage' => 66,
+            'Bezel set' => 64,
+            'Halo' => 65,
+            'Bypass' => 62,
+            'Pave' => 60,
+            'Solitaire' => 59,
+            'Modern'    => 89,
+            'Channel'   => 61
+        );
 
         //WOMAN WEDDING RINGS
-        $this->category_arr = array(
-            'Classic' => 77,
-            'Stackable' => 78,
-            'Eternity' => 81,
-            'Diamond' => 79
-        );
+//        $this->category_arr = array(
+//            'Classic' => 77,
+//            'Stackable' => 78,
+//            'Eternity' => 81,
+//            'Diamond' => 79
+//        );
 
         //FINE JEWERLY
 //        $this->category_arr = array(
@@ -227,7 +227,9 @@ class ControllerModuleExelParser extends Controller {
         //$filePath = '/home/canary/www/diamonds_rings_jewelry.csv';
         //$filePath = '/home/canary/www/new_engagement_rings.csv';
         //$filePath = '/home/canary/www/matching bands.csv';
-        $filePath = '/home/brilliantcanary/htdocs/matching bands.csv';
+        //$filePath = '/home/canary/www/solitaire engagement rings.csv';
+        $filePath = '/home/brilliantcanary/htdocs/solitaire engagement rings.csv';
+        //$filePath = '/home/brilliantcanary/htdocs/matching bands.csv';
         //$filePath = '/home/brilliantcanary/htdocs/new_engagement_rings.csv';
         //$filePath = '/home/brilliantcanary/htdocs/diamonds_rings_jewelry.csv';
         //$filePath = '/home/brilliantcanary/htdocs/fashion_jewelry.csv';
@@ -261,7 +263,7 @@ class ControllerModuleExelParser extends Controller {
                 //METAL
                 if ($metal == '14K White Gold' OR $metal == '14k White Gold') {
                     $this->metal = $this->list_metal['14K White Gold'];
-                    $this->sku = $curent[1] . 'W14';
+                    $this->sku = $curent[1] . '-W14';
                     $this->filter[] = $this->list_filtr['14K White Gold'];
 
                     $name_file_general_img = $sku.'.jpg';
@@ -269,49 +271,49 @@ class ControllerModuleExelParser extends Controller {
 
                 } elseif ($metal == '14K Yellow Gold' OR $metal == '14k Yellow Gold') {
                     $this->metal = $this->list_metal['14K Yellow Gold'];
-                    $this->sku = $curent[1] . 'Y14';
+                    $this->sku = $curent[1] . '-Y14';
                     $this->filter[] = $this->list_filtr['14K Yellow Gold'];
 
                     $name_file_general_img = $sku.'.alt.jpg';
 
                 } elseif ($metal == '14K Rose Gold' OR $metal == '14k Rose Gold') {
                     $this->metal = $this->list_metal['14K Rose Gold'];
-                    $this->sku = $curent[1] . 'R14';
+                    $this->sku = $curent[1] . '-R14';
                     $this->filter[] = $this->list_filtr['14K Rose Gold'];
 
                     $name_file_general_img = $sku.'.alt1.jpg';
 
                 } elseif ($metal == '18K White Gold' OR $metal == '18k White Gold') {
                     $this->metal = $this->list_metal['18K White Gold'];
-                    $this->sku = $curent[1] . 'W18';
+                    $this->sku = $curent[1] . '-W18';
                     $this->filter[] = $this->list_filtr['18K White Gold'];
 
                     $name_file_general_img = $sku.'.jpg';
 
                 } elseif ($metal == '18K Yellow Gold' OR $metal == '18k Yellow Gold') {
                     $this->metal = $this->list_metal['18K Yellow Gold'];
-                    $this->sku = $curent[1] . 'Y18';
+                    $this->sku = $curent[1] . '-Y18';
                     $this->filter[] = $this->list_filtr['18K Yellow Gold'];
 
                     $name_file_general_img = $sku.'.alt.jpg';
 
                 } elseif ($metal == '18K Rose Gold' OR $metal == '18k Rose Gold') {
                     $this->metal = $this->list_metal['18K Rose Gold'];
-                    $this->sku = $curent[1] . 'R18';
+                    $this->sku = $curent[1] . '-R18';
                     $this->filter[] = $this->list_filtr['18K Rose Gold'];
 
                     $name_file_general_img = $sku.'.alt1.jpg';
 
                 } elseif ($metal == 'Platinum') {
                     $this->metal = $this->list_metal['Platinum'];
-                    $this->sku = $curent[1] . 'PL';
+                    $this->sku = $curent[1] . '-PL';
                     $this->filter[] = $this->list_filtr['Platinum'];
 
                     $name_file_general_img = $sku.'.jpg';
 
                 } elseif ($metal == 'Palladium') {
                     $this->metal = $this->list_metal['Palladium'];
-                    $this->sku = $curent[1] . 'PA';
+                    $this->sku = $curent[1] . '-PA';
                     $this->filter[] = $this->list_filtr['Palladium'];
 
                     $name_file_general_img = $sku.'.jpg';
@@ -323,10 +325,10 @@ class ControllerModuleExelParser extends Controller {
 
 
                 //ENGAGEMENT RINGS
-                //$this->category[] = 20;
+                $this->category[] = 20;
 
                 //WOMAN WEDDING RINGS
-                $this->category[] = 69;
+                //$this->category[] = 69;
 
                 //FINE JEWERLY
                 //$this->category[] = 82;
