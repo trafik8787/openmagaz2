d26cf627e8394d19a9ed3130ab1571b84e010f07
Shtirlic <trafik8787@gmail.com>
2016-10-22 20:49:58 +0300

    fix blog home

--- catalog/controller/module/bloges.php
+++ catalog/controller/module/bloges.php
@@ -13,23 +13,36 @@ class ControllerModuleBloges extends Controller {
         $data = array();
         $result_arr = array();
 
-//        $rss = new DOMDocument();
-//        $rss->load($this->config->get('bloges_url'));
-//
-//        foreach ($rss->getElementsByTagName('item') as $node) {
-//
-//            $result_arr[] = array(
-//                'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
-//                'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
-//                'date_m' => date('M', strtotime($node->getElementsByTagName('pubDate')->item(0)->nodeValue)),
-//                'date_d' => date('d', strtotime($node->getElementsByTagName('pubDate')->item(0)->nodeValue))
-//
-//            );
-//            dd($node->getElementsByTagName('encoded')->item(0)->nodeValue);
-//        }
+        $this->load->model('tool/image');
+
+        $rss = new DOMDocument();
+        $rss->load($this->config->get('bloges_url'));
+
+        if (empty($this->cache->get('bloges'))) {
+
+            foreach ($rss->getElementsByTagName('item') as $node) {
+
+                $result_arr[] = array(
+                    'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+                    'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+                    'date_m' => date('M', strtotime($node->getElementsByTagName('pubDate')->item(0)->nodeValue)),
+                    'date_d' => date('d', strtotime($node->getElementsByTagName('pubDate')->item(0)->nodeValue)),
+                    'desk' => text::limit_chars(strip_tags($node->getElementsByTagName('encoded')->item(0)->nodeValue), 160 , null, true),
+                    'img' => $node->getElementsByTagName('content')->item(0)->getAttribute('url')
+                );
+                //dd($node->getElementsByTagName('content')->item(0)->getAttribute('url'));
+                //dd($this->model_tool_image->resize($node->getElementsByTagName('content')->item(0)->getAttribute('url'), 285, 173));
+            }
+
+            $this->cache->set('bloges', $result_arr);
+
+        } else {
+            $result_arr = $this->cache->get('bloges');
+        }
+
+        $data['bloges'] = $result_arr;
 
 
-//        dd($movies->channel);
         if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/bloges.tpl')) {
             return $this->load->view($this->config->get('config_template') . '/template/module/bloges.tpl', $data);
         } else {
