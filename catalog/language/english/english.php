7420d589fd859a7b37fa94350c2e46c862786c45
Shtirlic <trafik8787@gmail.com>
2016-11-19 16:15:17 +0200

    fix order date

--- catalog/language/english/english.php
+++ catalog/language/english/english.php
@@ -2,7 +2,7 @@
 // Locale
 $_['code']                  = 'en';
 $_['direction']             = 'ltr';
-$_['date_format_short']     = 'd/m/Y';
+$_['date_format_short']     = 'm/d/Y';
 $_['date_format_long']      = 'l dS F Y';
 $_['time_format']           = 'h:i:s A';
 $_['datetime_format']       = 'd/m/Y H:i:s';
