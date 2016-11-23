817362a58d9c70ec850108d1489f6cdc1971780e
Shtirlic <trafik8787@gmail.com>
2016-11-24 01:57:34 +0200

    fix test add akcia

--- catalog/controller/account/test.php
+++ catalog/controller/account/test.php
@@ -7,7 +7,7 @@
  */
 class ControllerAccountTest extends Controller {
 
-    public function index () {
+    public function rating () {
         $query = $this->db->query("SELECT product_id FROM " . DB_PREFIX . "product");
 
         foreach ($query->rows as $row) {
@@ -15,4 +15,24 @@ class ControllerAccountTest extends Controller {
             VALUES (".$row['product_id'].", 0, '', '', ".random_int(4, 5).", 1, NOW())");
         }
     }
+
+    public function akciya () {
+        //dd($this->request->get['cat'], true);
+        if (!empty($this->request->get['cat']) and !empty($this->request->get['procent'])) {
+
+            $cat = $this->request->get['cat'];
+            $query = $this->db->query("SELECT pr.product_id, pr.price FROM " . DB_PREFIX . "product pr INNER JOIN " . DB_PREFIX . "product_to_category ptc ON (pr.product_id = ptc.product_id) WHERE ptc.category_id = ".$cat);
+
+            foreach ($query->rows as $row) {
+
+                $tmp = $row['price'] * $this->request->get['procent'];
+                $special = $row['price'] - $tmp;
+
+                $qwe = $this->db->query("INSERT INTO ". DB_PREFIX ."product_special (product_id, customer_group_id, priority, price) VALUES (".$row['product_id'].", 1, 0, ".$special.")");
+                dd($qwe);
+            }
+        }
+
+    }
+
 }
