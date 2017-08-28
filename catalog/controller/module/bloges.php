660979ce6f6da467600231114113d2a9c4148082
Shtirlic <trafik8787@gmail.com>
2017-08-28 12:40:58 +0300

    blogs img copy in directory bloges_img_tmp

--- catalog/controller/module/bloges.php
+++ catalog/controller/module/bloges.php
@@ -27,37 +27,6 @@ class ControllerModuleBloges extends Controller {
     public function getBloges () {
 
 
-//        $ch = curl_init();
-//        curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-//        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-//        curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
-//        $headers = array( 'Expect:','Connection: Keep-Alive','Accept-Charset: utf-8,windows-1251;q=0.7,*;q=0.7' );
-//        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-//        curl_setopt($ch, CURLOPT_URL, $this->config->get('bloges_url')); // Адрес RSS
-//
-//        $rss_str = curl_exec($ch);
-//        if(!$rss_str) {
-//            print "Oooops. Can't get rss stream.\n";
-//            exit;
-//        }
-//
-//        $rss = simplexml_load_string($rss_str, 'SimpleXMLElement', LIBXML_NOCDATA);
-////        dd($rss, true);
-//
-//        foreach ($rss->channel->item as $node) {
-//            $result_arr[] = array(
-//                'title' => $node->title,
-//                'link' => $node->link,
-//                'date_m' => date('M', strtotime($node->pubDate)),
-//                'date_d' => date('d', strtotime($node->pubDate)),
-//                'desk' => text::limit_chars(strip_tags($node->description), 160 , null, true),
-//                'img' => $node->getElementsByTagName('content')->item(0)->getAttribute('url')
-//            );
-//        }
-
-
-
         $result_arr = array();
         $this->load->model('tool/image');
 
@@ -69,13 +38,21 @@ class ControllerModuleBloges extends Controller {
 
             foreach ($rss->getElementsByTagName('item') as $node) {
 
+                $url = $node->getElementsByTagName('content')->item(0)->getAttribute('url');
+                $name_img = basename($url);
+                $url_img = copy($url, $_SERVER['DOCUMENT_ROOT'].'/image/bloges_img_tmp/'.$name_img);
+
+                if ($url_img === true) {
+                    $url = '/image/bloges_img_tmp/'.$name_img;
+                }
+
                 $result_arr[] = array(
                     'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
                     'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
                     'date_m' => date('M', strtotime($node->getElementsByTagName('pubDate')->item(0)->nodeValue)),
                     'date_d' => date('d', strtotime($node->getElementsByTagName('pubDate')->item(0)->nodeValue)),
                     'desk' => text::limit_chars(strip_tags($node->getElementsByTagName('encoded')->item(0)->nodeValue), 160 , null, true),
-                    'img' => $node->getElementsByTagName('content')->item(0)->getAttribute('url')
+                    'img' => $url
                 );
                 //dd($node->getElementsByTagName('content')->item(0)->getAttribute('url'));
                 //dd($this->model_tool_image->resize($node->getElementsByTagName('content')->item(0)->getAttribute('url'), 285, 173));
@@ -86,6 +63,9 @@ class ControllerModuleBloges extends Controller {
         } else {
             $result_arr = $this->cache->get('bloges');
         }
+
+       // dd($result_arr, true);
+        //dd(phpinfo(), true);
 
         return $result_arr;
 
